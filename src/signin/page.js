c460bda8624272625b84e73996e066e85555ba5e
ZwaneProgram <phuwasit.wantaya@gmail>
2024-07-05 11:37:43 +0700

    Xtreme

---
+++ src/signin/page.js
@@ -0,0 +1,6 @@
+export default function signin() {
+    return (
+      <h1>This is signIn</h1>
+    );
+  }
+  
---
+++ src/signup/page.js
@@ -0,0 +1,6 @@
+export default function signup() {
+    return (
+      <h1>This is signUp</h1>
+    );
+  }
+  
