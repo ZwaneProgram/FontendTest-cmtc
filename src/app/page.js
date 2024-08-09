e8adb03bf9e22472e498cbfecff1d2861bf94562
ZwaneProgram <phuwasit.wantaya@gmail>
2024-08-09 12:32:15 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import TheCarousel from "./components/Carousel" 
+import Card from './components/Card'
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      <TheCarousel />
+      <Card />
+    </div>
   );
 }
